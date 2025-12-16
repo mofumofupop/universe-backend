d0380f43223d0e11ce5e6c710e3f9448f2e00a32
otoneko1102 <pineapple2006.11.2@gmail.com>
2025-12-17 08:42:28 +0900

    `/api/user` を実装

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { accountHandler } from "./routes/account.js";
 import { qrHandler } from "./routes/qr.js";
 import { exchangeHandler } from "./routes/exchange.js";
 import { iconHandler } from "./routes/icon.js";
+import { userHandler } from "./routes/user.js";
 
 const app = new Hono();
 
@@ -21,5 +22,6 @@ app.post("/api/account", accountHandler);
 app.post("/api/qr", qrHandler);
 app.post("/api/exchange", exchangeHandler);
 app.post("/api/icon", iconHandler);
+app.get("/api/user", userHandler);
 
 export default app;
