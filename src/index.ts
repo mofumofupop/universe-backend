47e67456c775a9ed3fe1545cfcbd62e8778bfe46
otoneko1102 <pineapple2006.11.2@gmail.com>
2025-12-19 18:37:25 +0900

    Delete cors

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,15 @@ import { exchangeHandler } from "./routes/exchange.js";
 import { iconHandler } from "./routes/icon.js";
 import { userHandler } from "./routes/user.js";
 import { loginHandler } from "./routes/login.js";
-import { cors } from "hono/cors";
-
+// import { cors } from "hono/cors";
 const app = new Hono();
 
-app.use(
-  "*",
-  cors({
-    origin: "*",
-  })
-);
+// app.use(
+//   "*",
+//   cors({
+//     origin: "*",
+//   })
+// );
 
 app.onError((err, c) => {
   console.error(err);
