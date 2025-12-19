4d6044800b608886fd1e59c4a72dd620e1f94c6e
otoneko1102 <pineapple2006.11.2@gmail.com>
2025-12-19 17:34:41 +0900

    CORSを修正

--- api/[[...route]].ts
+++ api/[[...route]].ts
@@ -6,19 +6,4 @@ export const config = {
   maxDuration: 30,
 };
 
-function withCors(handler: { (req: Request): Response | Promise<Response>; (arg0: any, arg1: any): any; }) {
-  return async (req: { method: string; }, res: { setHeader: (arg0: string, arg1: string) => void; statusCode: number; end: () => void; }) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    if (req.method === "OPTIONS") {
-      res.statusCode = 204;
-      res.end();
-      return;
-    }
-    return handler(req, res);
-  };
-}
-
-export default withCors(handle(app));
+export default handle(app);
